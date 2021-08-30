ce841dccbe5cdba4f069d30c4a5447b02bc58186
leandro <lgomesb@gmail.com>
2021-08-30 11:29:23 -0300

    Definido a insercao de exemplos para teste.

--- customer-maintainer/src/main/java/br/com/learning/customermaintainer/CustomerMaintainerApplication.java
+++ customer-maintainer/src/main/java/br/com/learning/customermaintainer/CustomerMaintainerApplication.java
@@ -1,13 +1,37 @@
 package br.com.learning.customermaintainer;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CustomerMaintainerApplication {
+import br.com.learning.customermaintainer.domain.Customer;
+import br.com.learning.customermaintainer.repositories.CustomerRepository;
 
+@SpringBootApplication
+public class CustomerMaintainerApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerMaintainerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		List<Customer> customers = Arrays.asList(
+				new Customer(null, "Fulando de Tal", "12324345657", LocalDate.now()),
+				new Customer(null, "Cicrano da Silva", "76652545436", LocalDate.now()),
+				new Customer(null, "João da Silva", "99888745436", LocalDate.now())
+				);
+		
+		repository.saveAll(customers);
 	}
 
 }
